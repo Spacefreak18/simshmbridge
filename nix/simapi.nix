6cf961720dc0f9bdff6453a832532671b55d6c23
Samuel Rounce <me@samuelrounce.co.uk>
2024-12-31 03:47:11 +0000

    Add Nix flake

---
+++ nix/simapi.nix
@@ -0,0 +1,7 @@
+{ fetchFromGitHub }:
+fetchFromGitHub {
+  owner = "spacefreak18";
+  repo = "simapi";
+  rev = "7595e2518bade21dc96b303d80965d0dfbf63254";
+  hash = "sha256-iNXRz+84IwD30UjDWyzjWrnQGKDgciNnxbxM50CKFG8=";
+}
